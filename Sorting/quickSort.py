5733872c0b7e5a38f278104e2b85103d547c0392
VasuPatel001 <vgpatel1797@gmail.com>
2023-05-10 14:26:38 -0500

    QuickSort updated

--- Sorting/quickSort.py
+++ Sorting/quickSort.py
@@ -1,52 +1,35 @@
 # Quick sort in Python
 
-# function to find the partition position
-def partition(array, low, high):
+import random
+class Solution(object):
+    def quickSort(self, nums) -> list[int]:
+        """
+        Parameters
+        ----------
+        nums : List[int]
+        :rtype: List[int]
+        """
+        def helper(A, start, end):
+            if start >= end:
+                return
+            
+            # random pivot choice
+            pivot_idx = random.randint(start, end)
+            A[pivot_idx], A[start] = A[start], A[pivot_idx]
+            
+            # partition function
+            smaller = start
+            for bigger in (start+1, end+1):
+                if A[bigger] < A[start]:
+                    smaller += 1
+                    A[smaller], A[bigger] = A[bigger], A[smaller]
+            A[start], A[smaller] = A[smaller], A[start]
 
-    # choose the rightmost element as pivot
-    pivot = array[high]
-
-    # pointer for greater element
-    i = low - 1
-
-    # traverse through all elements
-    # compare each element with pivot
-    for j in range(low, high):
-        if array[j] <= pivot:
-            # if element smaller than pivot is found
-            # swap it with the greater element pointed by i
-            i = i + 1
-
-            # swapping element at i with element at j
-            (array[i], array[j]) = (array[j], array[i])
-
-    # swap the pivot element with the greater element specified by i
-    (array[i + 1], array[high]) = (array[high], array[i + 1])
-
-    # return the position from where partition is done
-    return i + 1
-
-
-def helper(array, low, high):
-    if low >= high:
-        return
-
-    # find pivot element such that
-    # element smaller than pivot are on the left
-    # element greater than pivot are on the right
-    pi = partition(array, low, high)
-
-    # recursive call on the left of pivot
-    quickSort(array, low, pi - 1)
-
-    # recursive call on the right of pivot
-    quickSort(array, pi + 1, high)
-
-
-def quickSort(A: list[int]) -> list[int]:
-    helper(A, 0, len(A) - 1)
-    return A
-
+            # recurse on left and right partitions
+            helper(A, start, smaller - 1)
+            helper(A, smaller + 1, end)
+        
+        helper(A, 0, len(nums) - 1)
 
 A = [8, 7, 2, 1, 0, 9, 6]
 print("Unsorted Array")
