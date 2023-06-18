e902d24bb4b57c51ee95f50df960ff02e7a837cc
VasuPatel001 <vgpatel1797@gmail.com>
2023-06-18 09:47:06 -0500

    graph update

--- Graphs/bfsPseudoCode.py
+++ Graphs/bfsPseudoCode.py
@@ -1,19 +1,22 @@
 import queue
+from collections import deque
+
 
 class Graph:
     def bfs(source):
         # visited and parent list initialized to 0 and null respectively
         visited[source] = 1
 
-        q = queue.Queue(maxsize=0)
-        q.put(source)
+        # q = queue.Queue(maxsize=0)
+        q = deque()
+        q.append(source)
 
-        while not q.empty():
-            node = q.get()
+        while q:
+            node = q.popleft()
 
-            for neighbor in adjacencyList(node):
+            for neighbor in adjacencyList[node]:
                 if visited[neighbor] == -1:
                     visited[neighbor] = 1
                     parent[neighbor] = source
-                    q.put(neighbor)
+                    q.append(neighbor)
         
