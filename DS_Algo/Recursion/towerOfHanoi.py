3b12476402fa88f108543cfe7e3bb2ae8b99ec61
VasuPatel001 <vgpatel1797@gmail.com>
2024-10-17 09:03:22 -0500

    tower of hanoi update

--- DS_Algo/Recursion/towerOfHanoi.py
+++ DS_Algo/Recursion/towerOfHanoi.py
@@ -1,12 +1,20 @@
 def towerOfHanoi(n: int, src, dst, aux, moves) -> int:
     if n <= 1:
         return 1
-    moves += towerOfHanoi(n-1, src, dst, aux)
+    moves += towerOfHanoi(n-1, src, dst, aux, moves)
     moves += 1  # 1 move required to move nth disk from src to dst
-    moves += towerOfHanoi(n-1, aux, dst, src)
+    moves += towerOfHanoi(n-1, aux, dst, src, moves)
     return moves
 
+
 def movesForTowerOfHanoi(n):
-    src, dst, aux
+    src = []
+    dst = []
+    aux = []
     moves = 0
     return towerOfHanoi(n, src, dst, aux, moves)
+
+
+if __name__ == "__main__":
+    n = 5
+    print(movesForTowerOfHanoi(n))
