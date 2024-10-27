f8643a0752d52b28df8798550047e347c1d1d55c
VasuPatel001 <vgpatel1797@gmail.com>
2024-10-27 17:53:17 -0500

    sorting update

--- DS_Algo/Sorting/252_meetingRoomsI.py
+++ DS_Algo/Sorting/252_meetingRoomsI.py
@@ -1,6 +1,28 @@
+"""
+Leetcode 252:  Meeting Rooms
+Easy
+
+Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+Example 1:
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: false
+
+Example 2:
+Input: intervals = [[7,10],[2,4]]
+Output: true
+
+Constraints:
+0 <= intervals.length <= 104
+intervals[i].length == 2
+0 <= starti < endi <= 106
+"""
+
+
 class Solution:
-    def canAttendMeetings(meetings: list[list[int]]):
-        for i in range(len(meetings)-1):
-            if meetings[i][1] > meetings[i+1][0]:
+    def canAttendMeetings(self, intervals: list[list[int]]) -> bool:
+        intervals.sort()
+        for i in range(len(intervals)-1):
+            if intervals[i][1] > intervals[i+1][0]:
                 return False
         return True
