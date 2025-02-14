ed388714c21b91bff5d782610b152514106db7d1
VasuPatel001 <vgpatel1797@gmail.com>
2025-02-14 12:23:56 -0600

    heappq.heapify() NOTE

--- DS_Algo/Sorting/heapSort.py
+++ DS_Algo/Sorting/heapSort.py
@@ -2,6 +2,7 @@ import heapq
 
 
 def heapSort(nums: list[int]) -> list[int]:
+    # NOTE: heappq.heapify() converts list to heap priority queue in-place in time O(n) hence we don't store in a new variable
     heapq.heapify(nums)  # note this is min heapq or min priority queue
     result = []
     while nums:
